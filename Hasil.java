ef6d26109726cf9d3e2a9b22a132f27c62d82f3f
Mohammad Alka. R <99939707+alfkarmdhn@users.noreply.github.com>
2022-04-04 20:12:53 +0700

    Create Hasil.java

---
+++ Hasil.java
@@ -0,0 +1,22 @@
+
+package pewarisanayahanak;
+
+public class Hasil {
+    
+    public static void main(String[] args) {
+      
+        
+        Anak anak=new Anak();
+        anak.Cetak();
+        System.out.println("");
+        
+        Cucu cucu=new Cucu();
+        cucu.Cetak();
+        System.out.println("");
+        
+        Cicit cicit=new Cicit();
+        cicit.Cetak();
+        System.out.println("");
+    }
+
+}
